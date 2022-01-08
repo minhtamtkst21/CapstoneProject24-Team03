a87206699662e45341d74af9b9b0f5a6ad13d56c
187PM13938 - Nguyen Huynh Quoc Khang - K24T-PM1 <khang.187pm13938@vanlanguni.vn>
2022-01-09 03:12:57 +0700

    Create Kiểm Tra File

--- Cap24Team3/Areas/Faculty/Controllers/DiemHocPhansController.cs
+++ Cap24Team3/Areas/Faculty/Controllers/DiemHocPhansController.cs
@@ -22,7 +22,12 @@ namespace Cap24Team3.Areas.Faculty.Controllers
             HttpPostedFileBase file = Request.Files["UploadedFile"];
             if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
             {
-
+                var listError = KiemTraFile(file);
+                if (listError != "")
+                {
+                    TempData["Alert"] = listError;
+                    return Redirect(Request.UrlReferrer.ToString());
+                }
                 string fileName = file.FileName;
                 string fileContentType = file.ContentType;
                 byte[] fileBytes = new byte[file.ContentLength];
@@ -93,17 +98,179 @@ namespace Cap24Team3.Areas.Faculty.Controllers
                             //    //savesv.LichSuUpLoad = db.LichSuUpLoads.FirstOrDefault(s => s.ThoiGian == timelichsu);
                             //}
                             db.DiemHocPhans.Add(savediem);
-                            db.SaveChanges();
+                            //db.SaveChanges();
                         }
 
                     }
                 }
             }
+            else
+            {
+                TempData["Alert"] = "File bị trống, vui lòng thử lại!!";
+            }
 
-            //db.Configuration.AutoDetectChangesEnabled = false;
-            //db.Configuration.ValidateOnSaveEnabled = false;
-            //db.SaveChanges();
+            db.Configuration.AutoDetectChangesEnabled = false;
+            db.Configuration.ValidateOnSaveEnabled = false;
+            db.SaveChanges();
             return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        //Kiểm tra lỗi của file excel import vào.
+        public string KiemTraFile(HttpPostedFileBase file)
+        {
+            string DanhSachLoi = "";
+            byte[] fileBytes = new byte[file.ContentLength];
+            var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+            using (var package = new ExcelPackage(file.InputStream))
+            {
+                var currentSheet = package.Workbook.Worksheets;
+                var workSheet = currentSheet.First();
+                if (workSheet.Dimension != null)
+                {
+                    var noOfRow = workSheet.Dimension.End.Row;
+                    var noOfCol = workSheet.Dimension.End.Column;
+                    if (noOfCol == 11 && noOfRow > 1)
+                    {
+                        
+                        for(int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            DiemHocPhan savediem = new DiemHocPhan();
+                            if (workSheet.Cells[rowIterator, 1].Value != null)
+                            {
+                                savediem.MSSV = workSheet.Cells[rowIterator, 1].Value.ToString();
+                                
+                                if (savediem.MSSV.Length > 20)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột A: Dữ liệu không được quá 20 ký tự, số ký tự của " + savediem.MSSV + " là " + savediem.MSSV.Length + "<p>";
+                                }
+                                if (savediem.MSSV.Replace(" ", string.Empty) == null)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột A: Dữ liệu không được để trống <p>";
+                                }
+                                
+
+                            }
+                            else
+                            {
+                                DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột A: Dữ liệu không được để trống <p>";
+                            }
+                            if (workSheet.Cells[rowIterator, 3].Value != null)
+                            {
+                                savediem.HocKy = int.Parse(workSheet.Cells[rowIterator, 3].Value.ToString().Replace("HK", string.Empty));
+                                if (savediem.HocKy <= 0)
+                                {
+                                    DanhSachLoi += "<p> Học Kì đào tạo phải là số nguyên dương lớn hơn 0, khóa đào tạo bạn nhập là: " + savediem.HocKy + "</p>";
+                                }
+                                if (savediem.HocKy == null)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột C: Dữ liệu không được để trống <p>";
+                                }
+                               
+                            }
+
+                            if (workSheet.Cells[rowIterator, 4].Value != null)
+                            {
+                                savediem.HocPhan = workSheet.Cells[rowIterator, 4].Value.ToString();
+                                if (savediem.HocPhan.Length > 7)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột D: Dữ liệu không được quá 7 ký tự, số ký tự của " + savediem.HocPhan + " là " + savediem.HocPhan.Length + "<p>";
+                                }
+                                if (savediem.HocPhan.Replace(" ", string.Empty) == null)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột D: Dữ liệu không được để trống <p>";
+                                }
+                                
+
+                            }
+                            if (workSheet.Cells[rowIterator, 6].Value != null)
+                            {
+                                savediem.TenHocPhan = workSheet.Cells[rowIterator, 6].Value.ToString();
+                                if (savediem.TenHocPhan.Replace(" ", string.Empty) == null)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột F: Dữ liệu không được để trống <p>";
+                                }
+                            }
+
+                            if (workSheet.Cells[rowIterator, 7].Value != null)
+                            {
+                                savediem.SoTinChi = int.Parse(workSheet.Cells[rowIterator, 7].Value.ToString().Trim());
+                                
+                                if (savediem.SoTinChi == null)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột G: Dữ liệu không được để trống <p>";
+                                }
+                                if (savediem.SoTinChi <=0)
+                                {
+                                    DanhSachLoi += "<p> Số tín chỉ phải là số nguyên dương lớn hơn 0, khóa đào tạo bạn nhập là: " + savediem.SoTinChi + "</p>";
+                                }
+
+                            }
+
+                            if (workSheet.Cells[rowIterator, 8].Value != null)
+                            {
+                                savediem.Diem10 = workSheet.Cells[rowIterator, 8].Value.ToString();
+                                if (savediem.Diem10.Length > 3)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột H: Dữ liệu không được quá 3 ký tự, số ký tự của " + savediem.Diem10 + " là " + savediem.Diem10.Length + "<p>";
+                                }
+                                
+                            }
+                            if (workSheet.Cells[rowIterator, 9].Value != null)
+                            {
+                                savediem.Diem4 = workSheet.Cells[rowIterator, 9].Value.ToString();
+                                if (savediem.Diem4.Length > 4)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột I: Dữ liệu không được quá 4 ký tự, số ký tự của " + savediem.Diem4 + " là " + savediem.Diem4.Length + "<p>";
+                                }
+                                
+                            }
+                            if (workSheet.Cells[rowIterator, 10].Value != null)
+                            {
+                                savediem.DiemChu = workSheet.Cells[rowIterator, 10].Value.ToString();
+                                if (savediem.DiemChu.Length > 2)
+                                {
+                                    DanhSachLoi += "<p> Lỗi ở dòng " + rowIterator + ", cột J: Dữ liệu không được quá 2 ký tự, số ký tự của " + savediem.DiemChu + " là " + savediem.DiemChu.Length + "<p>";
+                                }
+                                
+                            }
+                            if (workSheet.Cells[rowIterator, 11].Value != null)
+                            {
+                                var quamon = workSheet.Cells[rowIterator, 11].Value.ToString().Trim();
+                                if (quamon == "x")
+                                {
+                                    savediem.QuaMon = true;
+                                }
+                                else
+                                {
+                                    savediem.QuaMon = false;
+                                }
+                            }
+                        }    
+                    }
+                    else if (noOfCol != 11)
+                    {
+                        DanhSachLoi += "<p> File bị sai định dạng, vui lòng thử lại!!</p>";
+                    }
+                    else
+                    {
+                        DanhSachLoi += "<p> File bị trống, vui lòng thử lại!!</p>";
+                    }
+                }
+            }
+            return DanhSachLoi;
+        }
+
+        //Check
+        public bool CheckTonTai(string element, List<string> list)
+        {
+            foreach (var item in list)
+            {
+                if (element == item)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         // GET: Faculty/DiemHocPhans
@@ -111,105 +278,6 @@ namespace Cap24Team3.Areas.Faculty.Controllers
         {
             var diemHocPhans = db.DiemHocPhans.Include(d => d.LichSuUpLoad);
             return View(diemHocPhans.ToList());
-        }
-
-        // GET: Faculty/DiemHocPhans/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiemHocPhan diemHocPhan = db.DiemHocPhans.Find(id);
-            if (diemHocPhan == null)
-            {
-                return HttpNotFound();
-            }
-            return View(diemHocPhan);
-        }
-
-        // GET: Faculty/DiemHocPhans/Create
-        public ActionResult Create()
-        {
-            ViewBag.LichSu = new SelectList(db.LichSuUpLoads, "ID", "NguoiUpload");
-            return View();
-        }
-
-        // POST: Faculty/DiemHocPhans/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,MSSV,HocPhan,TenHocPhan,HocKy,SoTinChi,Diem10,Diem4,DiemChu,QuaMon,LichSu")] DiemHocPhan diemHocPhan)
-        {
-            if (ModelState.IsValid)
-            {
-                db.DiemHocPhans.Add(diemHocPhan);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.LichSu = new SelectList(db.LichSuUpLoads, "ID", "NguoiUpload", diemHocPhan.LichSu);
-            return View(diemHocPhan);
-        }
-
-        // GET: Faculty/DiemHocPhans/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiemHocPhan diemHocPhan = db.DiemHocPhans.Find(id);
-            if (diemHocPhan == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.LichSu = new SelectList(db.LichSuUpLoads, "ID", "NguoiUpload", diemHocPhan.LichSu);
-            return View(diemHocPhan);
-        }
-
-        // POST: Faculty/DiemHocPhans/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,MSSV,HocPhan,TenHocPhan,HocKy,SoTinChi,Diem10,Diem4,DiemChu,QuaMon,LichSu")] DiemHocPhan diemHocPhan)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(diemHocPhan).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.LichSu = new SelectList(db.LichSuUpLoads, "ID", "NguoiUpload", diemHocPhan.LichSu);
-            return View(diemHocPhan);
-        }
-
-        // GET: Faculty/DiemHocPhans/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            DiemHocPhan diemHocPhan = db.DiemHocPhans.Find(id);
-            if (diemHocPhan == null)
-            {
-                return HttpNotFound();
-            }
-            return View(diemHocPhan);
-        }
-
-        // POST: Faculty/DiemHocPhans/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            DiemHocPhan diemHocPhan = db.DiemHocPhans.Find(id);
-            db.DiemHocPhans.Remove(diemHocPhan);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
