a5bdebb5a70431ef946971f5f76419c130c5ecf6
TamNguyen7567 <TamNguyen7567@DESKTOP-1CR4R04>
2022-01-10 00:30:01 +0700

    login

--- Cap24Team3/Controllers/AccountController.cs
+++ Cap24Team3/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace Cap24Team3.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +34,9 @@ namespace Cap24Team3.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -65,7 +65,6 @@ namespace Cap24Team3.Controllers
         // POST: /Account/Login
         [HttpPost]
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
         {
             if (!ModelState.IsValid)
@@ -120,7 +119,7 @@ namespace Cap24Team3.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -155,15 +154,15 @@ namespace Cap24Team3.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "HocPhanDaoTao");
                 }
                 AddErrors(result);
             }
@@ -275,7 +274,6 @@ namespace Cap24Team3.Controllers
         // POST: /Account/ExternalLogin
         [HttpPost]
         [AllowAnonymous]
-        [ValidateAntiForgeryToken]
         public ActionResult ExternalLogin(string provider, string returnUrl)
         {
             // Request a redirect to the external login provider
@@ -343,7 +341,35 @@ namespace Cap24Team3.Controllers
                     // If the user does not have an account, then prompt the user to create an account
                     ViewBag.ReturnUrl = returnUrl;
                     ViewBag.LoginProvider = loginInfo.Login.LoginProvider;
-                    return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = loginInfo.Email });
+                    var model = new ExternalLoginConfirmationViewModel { Email = loginInfo.Email };
+                    if (User.Identity.IsAuthenticated)
+                    {
+                        return RedirectToAction("Index", "Manage");
+                    }
+
+                    if (ModelState.IsValid)
+                    {
+                        // Get the information about the user from the external login provider
+                        var info = await AuthenticationManager.GetExternalLoginInfoAsync();
+                        if (info == null)
+                        {
+                            return View("ExternalLoginFailure");
+                        }
+                        var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                        var result1 = await UserManager.CreateAsync(user);
+                        if (result1.Succeeded)
+                        {
+                            result1 = await UserManager.AddLoginAsync(user.Id, info.Login);
+                            if (result1.Succeeded)
+                            {
+                                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                                return RedirectToLocal(returnUrl);
+                            }
+                        }
+                        AddErrors(result1);
+                    }
+                    ViewBag.ReturnUrl = returnUrl;
+                    return View(model);
             }
         }
 
@@ -392,7 +418,7 @@ namespace Cap24Team3.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "HocPhanDaoTao");
         }
 
         //
@@ -449,7 +475,7 @@ namespace Cap24Team3.Controllers
             {
                 return Redirect(returnUrl);
             }
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "HocPhanDaoTao");
         }
 
         internal class ChallengeResult : HttpUnauthorizedResult
--- Cap24Team3/Controllers/HocPhanDaoTaoController.cs
+++ Cap24Team3/Controllers/HocPhanDaoTaoController.cs
@@ -59,7 +59,7 @@ namespace WebApplication.Controllers
             }
             else
             {
-                TempData["Alert"] = "Bạn chưa đăng nhập! <a href=Account/SignIn><u>Nhấn vào đây</u></a> để đăng nhập";
+                TempData["Alert"] = "Bạn chưa đăng nhập!";
                 return View("Index");
             }
         }
